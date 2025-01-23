eb48e4df6b1889e24a06733d676e8969dc9e0563
Pramod Magesh <pramodmagesh20@gmail.com>
2025-01-23 13:08:09 +0530

    No changes

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
 app.get('/ping', (request, response) => {
     response.send('Pinging?');
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});

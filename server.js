fd3c3c451b054198421bdc607d942910e12ebf56
Pramod Magesh <pramodmagesh20@gmail.com>
2025-01-29 13:23:53 +0530

    Port default added

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 
 require("dotenv").config();
 
-const port = process.env.port;
+const port = process.env.port || 3000;
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);

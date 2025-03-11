1311a973af4937d5f08c2fceb0c78fa1c97b3be5
Pramod Magesh <pramodmagesh20@gmail.com>
2025-03-11 17:03:36 +0530

    Completed Update and Delete entity

--- client/src/components/EditName/EditName.jsx
+++ client/src/components/EditName/EditName.jsx
@@ -1,28 +1,35 @@
 import React, { useState } from 'react'
-import "./AddName.css"
 import axios from "axios"
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
-function AddName() {
+function EditName() {
 
-    const [information, setInformation] = useState({
-        name: "",
-        source: "",
-        creator: "",
-    });
-
+    const location = useLocation();
+    const user = location.state?.user || { name: "", source: "", creator: "", id: ""};
     const navigate = useNavigate();
 
+    const [information, setInformation] = useState({
+        name: user.name,
+        source: user.source, 
+        creator: user.creator,
+    });
+    const [id, setId] = useState(user._id); 
+    
     const handleSubmit = async (e) => {
         e.preventDefault();
+
+        if (!id) {
+            alert("Enter ID");
+            return
+        }
+
         try {
-            const response = await axios.post(`${import.meta.env.VITE_LINK}`, information);
-            console.log("Successfully added");
+            const response = await axios.put(`${import.meta.env.VITE_LINK}${id}`, information);
             console.log(response.data);
             navigate("/viewnames");
         }
         catch (error) {
-            console.log("Error in adding name", error);
+            console.log("Error updating data", error);
         }
     }
 
@@ -30,33 +37,37 @@ function AddName() {
         <>
             <div className="addMain">
                 <form className="addMainForm" onSubmit={handleSubmit}>
-                    <label> Name: </label>
+                    {/* <label> ID: </label>
                     <input type="text" onChange={(e) => {
+                        setId(e.target.value)
+                    }}/> */}
+                    <label> Name: </label>
+                    <input type="text" value={user.name} onChange={(e) => {
                         setInformation((previous) => ({
                             ...previous,
                             name: e.target.value,
                         }))
                     }}/>
                     <label> Source: </label>
-                    <input type="text" onChange={(e) => {
+                    <input type="text" value={user.source} onChange={(e) => {
                         setInformation((previous) => ({
                             ...previous,
                             source: e.target.value,
                         }))
                     }}/>
                     <label> Creator: </label>
-                    <input type="text" onChange={(e) => {
+                    <input type="text" value={user.creator} onChange={(e) => {
                         setInformation((previous) => ({
                             ...previous,
                             creator: e.target.value,
                         }))
                     }}/>
 
-                    <button type="submit" className="addNameSubmit"> Add Name </button>
+                    <button type="submit" className="addNameSubmit"> Update </button>
                 </form>
             </div>
         </>
     )
 }
 
-export default AddName
+export default EditName;
